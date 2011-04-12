e679be10595a9882462b06ad92cdc997ffa4f8bf
smallufo <smallufo@gmail.com>
2011-04-12 23:07:10 +0800

    .

--- src/test/java/destiny/data/gauquelin/SuccessfulFrench2154ReaderTest.java
+++ src/test/java/destiny/data/gauquelin/SuccessfulFrench2154ReaderTest.java
@@ -3,29 +3,29 @@
  */
 package destiny.data.gauquelin;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.inject.Inject;
 
-import junit.framework.TestCase;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 import destiny.data.GeolocationFinder;
 import destiny.data.GeolocationFinderYahooImpl;
-import destiny.utils.location.TimeZoneGisgraphyImpl;
-import destiny.utils.location.TimeZoneIF;
 import destiny.utils.location.TimeZoneService;
 import destiny.web.yahoo.AppData;
 
-public class SuccessfulFrench2154ReaderTest extends TestCase
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:core.xml"})
+public class SuccessfulFrench2154ReaderTest 
 {
   protected GeolocationFinder geolocationFinder;
 
+  @Inject
+  private TimeZoneService timeZoneService;
+  
   protected void _setUp() throws Exception
   {
     AppData yahooAppData = new AppData("nK_pLKTIkYXVXPGU4I26K_SDQ1PRSP2q" , "614990115076948b0825ec2dbb9a4530");
-    TimeZoneGisgraphyImpl timeZoneGisgraphyImpl = new TimeZoneGisgraphyImpl("http://db.xfiles.to:9090/geoloc/findnearbylocation");
-    List<TimeZoneIF> timeZoneImpls = new ArrayList<TimeZoneIF>();
-    timeZoneImpls.add(timeZoneGisgraphyImpl);
-    TimeZoneService timeZoneService = new TimeZoneService(timeZoneImpls);
-
     geolocationFinder = new GeolocationFinderYahooImpl(yahooAppData, timeZoneService);
   }
 
