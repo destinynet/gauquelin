614f8dc9f716ba577d64a30097d1147b5ff93590
smallufo <smallufo@192.168.1.108>
2012-02-28 01:55:39 +0800

    重寫 BirthAndDeath.txt(及其 reader), 改用 2009 年的版本。 該版本以 TAB 區隔欄位，比較好 parse

--- src/test/java/destiny/data/gauquelin/BirthAndDeathReader.java
+++ src/test/java/destiny/data/gauquelin/BirthAndDeathReader.java
@@ -1,17 +1,16 @@
 /**
- * @author smallufo 
- * Created on 2008/3/10 at 下午 4:35:39
- */ 
+ * @author smallufo
+ * Created on 2012/2/27 at 下午3:18:03
+ */
 package destiny.data.gauquelin;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.net.URL;
+import java.util.StringTokenizer;
 
 import javax.inject.Inject;
-
-import junit.framework.TestCase;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,83 +21,95 @@ import destiny.core.calendar.Location;
 import destiny.core.calendar.Location.EastWest;
 import destiny.core.calendar.Location.NorthSouth;
 import destiny.core.calendar.Time;
-import destiny.core.calendar.eightwords.EightWords;
 import destiny.core.calendar.eightwords.EightWordsContext;
 import destiny.utils.location.TimeZoneUtils;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"classpath:core.xml"})
-public class BirthAndDeathReader extends TestCase
+/** 讀取的是 http://cura.free.fr/gauq/902gdF1.html 
+ * 此為 2009 改版的格式 , 用 TAB 區隔欄位
+ * */
+public class BirthAndDeathReader
 {
   @Inject
   EightWordsContext eightWordsContext;
   
   @Test
-  public void testPrint8words()
+  public void testRead() throws Exception
   {
     URL url = getClass().getResource("BirthAndDeath.txt");
     File file;
     BufferedReader bReader = null;
     
-    try
+    file = new File(url.toURI());
+    FileReader fReader = new FileReader(file);
+    bReader = new BufferedReader(fReader);
+    String line = null;
+    Time birthTime = null;
+    Time deathTime = null;
+    
+    int i=0;
+    while ((line = bReader.readLine()) != null)
     {
-      file = new File(url.toURI());
-      FileReader fReader = new FileReader(file);
-      bReader = new BufferedReader(fReader);
-      String line = null;
-      Time birthTime = null;
-      Time deathTime = null;
-      
-      int i=0;
-      while ((line = bReader.readLine()) != null)
+      if (!line.equals("") && line.charAt(0) != '#')
       {
-        if (!line.equals("") && line.charAt(0) != '#')
+        StringTokenizer st = new StringTokenizer(line , "\t");
+        long num = Long.valueOf(st.nextToken()).longValue();
+        char birthOrDeath = st.nextToken().charAt(0);
+        int day = Integer.valueOf(st.nextToken().trim());
+        int month = Integer.valueOf(st.nextToken().trim());
+        int year = Integer.valueOf(st.nextToken().trim());
+        int hour = Integer.valueOf(st.nextToken().trim());
+        int minute = Integer.valueOf(st.nextToken().trim());
+        int second = Integer.valueOf(st.nextToken().trim());
+        if (st.countTokens() == 4)
+          st.nextToken(); // 跳過 Ci
+        int tz = Integer.valueOf(st.nextToken().trim());
+        tz = 0-tz; // CET 時間其實是 GMT+1 , 但是這兒傳回的都是 -1
+        
+        //應該都是 48N50
+        String lat = st.nextToken().trim();
+        StringTokenizer stNS = new StringTokenizer(lat,"NS");
+        int latDeg = Integer.valueOf(stNS.nextToken().trim());
+        char NS = lat.charAt(2);
+        int latMin = Integer.valueOf(stNS.nextToken().trim());
+        
+        //應該都是 2E20
+        String lon = st.nextToken();
+        StringTokenizer stEW = new StringTokenizer(lon,"EW");
+        int longDeg = Integer.valueOf(stEW.nextToken().trim());
+        char EW = lon.charAt(1);
+        int longMin = Integer.valueOf(stEW.nextToken().trim());
+        
+        
+        Time time = new Time(year , month , day , hour , minute , second);
+        Location location = new Location( (EW == 'E' ? EastWest.EAST : EastWest.WEST) , longDeg , longMin , 0 , (NS == 'N' ? NorthSouth.NORTH : NorthSouth.SOUTH) , latDeg , latMin , 0 , 0 , TimeZoneUtils.getTimeZone(tz*60) );
+        
+        if (birthOrDeath == 'B')
+          birthTime = time;
+        else
         {
-          char birthOrDeath = line.charAt(5);
-          int day = Integer.valueOf(line.substring(11, 13).trim());
-          int month = Integer.valueOf(line.substring(13, 15).trim());
-          int year = Integer.valueOf(line.substring(15, 19).trim());
-          int hour = Integer.valueOf(line.substring(19, 21).trim());
-          int minute = Integer.valueOf(line.substring(21, 23).trim());
-          int second = Integer.valueOf(line.substring(23, 25).trim());
-          int tz = Integer.valueOf(line.substring(25, 27).trim());
-          
-          int latDeg = Integer.valueOf(line.substring(27, 29).trim());
-          char NS = line.charAt(29);
-          int latMin = Integer.valueOf(line.substring(30, 32).trim());
-          
-          int longDeg = Integer.valueOf(line.substring(32, 34).trim());
-          char EW = line.charAt(34);
-          int longMin = Integer.valueOf(line.substring(35, 37).trim());
-          
-          
-          Time time = new Time(year , month , day , hour , minute , second);
-          Location location = new Location( (EW == 'E' ? EastWest.EAST : EastWest.WEST) , longDeg , longMin , 0 , (NS == 'N' ? NorthSouth.NORTH : NorthSouth.SOUTH) , latDeg , latMin , 0 , 0 , TimeZoneUtils.getTimeZone(tz*60) );
-          
-          if (birthOrDeath == 'B')
-            birthTime = time;
-          else
+          deathTime = time;
+          //計算存活多久
+          if (deathTime.isAfter(birthTime))
           {
-            deathTime = time;
-            //計算存活多久
-            if (deathTime.diffSeconds(birthTime) < 60*60*24*365)
-            {
-              i++;
-              EightWords ew1 = eightWordsContext.getEightWords(birthTime , location);
-              System.out.println("======"+i+"=====");
-              System.out.print("出生於 : " + ew1);
-
-              EightWords ew2 = eightWordsContext.getEightWords(deathTime , location);
-              System.out.println("\n卒於 : " + ew2);
-            }
+            i++;
+            System.out.println("======"+i+" ("+num+")===== : " + location );
           }
+          
+//          if (deathTime.diffSeconds(birthTime) < 60*60*24*365)
+//          {
+//            //列出小於一歲的
+//            i++;
+//            EightWords ew1 = eightWordsContext.getEightWords(birthTime , location);
+//            System.out.println("======"+i+" ("+num+")=====");
+//            System.out.print("出生於 : " + ew1);
+//
+//            EightWords ew2 = eightWordsContext.getEightWords(deathTime , location);
+//            System.out.println("\n卒於 : " + ew2);
+//          }
         }
-      }//while
-    }
-    catch (Exception e)
-    {
-      e.printStackTrace();
-    }
-  }
-  
+      }
+    }//while
+  } // test()
 }
